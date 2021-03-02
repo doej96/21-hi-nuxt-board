4d6edfa33b76899f8404ab68111f46a93e888fdd
doej96 <doej96@naver.com>
2021-03-02 11:42:51 +0900

    ,,,,,

--- components/Header.vue
+++ components/Header.vue
@@ -1,38 +1,37 @@
 <template lang="pug">
 	header.header-wrapper
-		.logo
-			i.fa.fa-sun
-			nuxt-link(to='/') Eunjeong
+		.logo.py-2.px-4
+			nuxt-link(to="/")
+				v-avatar(color="booldook" size="48") BK
 		ul.navi-wrapper
-			li.navi
-				nuxt-link(to='/') Nuxt-Board
-			li.navi
-				a(href='/http://127.0.0.1:3000/board') Express-Board
-			li.navi
-				a(href='/http://127.0.0.1:3000/gallery') Express-Gallery
+			.navi
+				a(href=`${process.env.SERVER}`) Node/Express Board
+			.navi
+				a(href=`${process.env.REACT}`) React/Nuxt Board
 </template>
+
 <script>
 export default {
-	name: 'Header',
+	name: "Headers"
 }
 </script>
+
 <style lang="scss" scoped>
-	.header-wrapper{
-		color: $colorLighter;
-		background-color: $colorDarker;
-		@include flex($FS, $CT);
-		.logo {
-			@include flex($FS, $CT);
-			padding: 1em;
-			i {
-				margin-right: 1em;
-			}
-		}
-		.navi-wrapper {
-			@include flex($FS, $CT);
-			.navi {
-				padding: 1em;
-			}
-		}
-	}
+.header-wrapper {
+	background-color: $colorDarker;
+	color: $colorLighter;
+  @include flex($SB, $CT);
+	border-bottom: 1px solid $colorLighter;
+	align-items: center;
+	.logo {
+		padding: 0 1.5em;
+  }
+	.navi-wrapper {
+    @include flex($SB, $CT);
+		align-items: center;
+		.navi {
+			padding: 0.75em 1.5em;
+    }
+  }
+}
 </style>
--- views/layouts/default.vue
+++ views/layouts/default.vue
@@ -1,31 +1,7 @@
 <template lang="pug">
-	div.container-wrapper
-		header.header-wrapper
-			h1 Header
-		Nuxt
-		footer.footer-wrapper
-			p copyright eunjeong
+	Nuxt
 </template>
-<script>
-export default {
-	
-}
-</script>
+
 <style lang="scss">
-	.container-wrapper {
-		height: 100vh;
-		@include flex($FS, $ST);
-		flex-direction: column;
-		.header-wrapper {
-			background-color: $colorBlack;
-			color: $colorWhite;
-		}
-		.next-wrapper {
-			flex-grow: 1;
-		}
-		.footer-wrapper {
-			background-color: $colorBlack;
-			color: $colorWhite;
-		}
-	}
+
 </style>
